5969a81b0da8bcd4a3c361a807419037dffe73a9
Sergey <ranik07@bk.ru>
2024-10-28 23:04:17 +0300

    28/10/2024

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -1,33 +1,39 @@
 package org.example;
 
-import java.util.Arrays;
-
 public class Main {
-    public static void main(String[] args) throws MyArraySizeException {
 
-        String[][] array = {{"5", "7", "3", "17"}, {"7", "0", "1", "12"}, {"8", "1", "2", "3"}, {"8", "5", "4", "3"}};
-        System.out.println(sumArray(array,4, 4 ));
+    public static void main(String[] args) throws MyArraySizeException, MyArrayDataException {
 
+        String[][] array = { { "5", "7", "3", "17" }, { "7", "0", "1", "12" }, { "8", "1", "2", "3" },
+                { "8", "5", "4", "3" } };
+
+        try {
+            System.out.println("Сумма элементов массива равна " + sumArray1(array, 4, 4));
+        } catch (MyArrayDataException | MyArraySizeException e) {
+            System.out.println("Не удалось выполнить расчет");
+            e.printStackTrace();
         }
+    }
 
-        public static int sumArray(String[][] array, int numberLines, int numberColumns) throws MyArraySizeException {
-        int sum =0;
-        if (array.length == numberLines){
-            for (int i=0; i < array[i].length; i++) {
-                System.out.println("i = " + i);
-                if (array[i].length != numberColumns) {
-                    throw new MyArraySizeException("Размер массива не соответсвует заданному размеру");
-                }
-                for (int j=0; j <= array[i].length; j++){
-                    System.out.println("j = " + j);
-                    System.out.println(sum);
-                    sum += Integer.parseInt(array[i][j]);
+    public static int sumArray1(String[][] array, int numberLines, int numberColumns) throws MyArraySizeException, MyArrayDataException{
+        int sum = 0;
+        if (array.length == numberLines) {
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].length == numberColumns) {
+                    for (int j = 0; j < array[i].length; j++) {
+                        try {
+                            sum += Integer.parseInt(array[i][j]);
+                        } catch (NumberFormatException e) {
+                            throw new MyArrayDataException("Данные в строке " + (i + 1) + " столбце " + (j+1) + " неудалось преобразовать к числовому значению");
+                        }
                     }
+                } else {
+                    throw new MyArraySizeException("Количество столбцов в " + (i + 1) + " строке не равно " + numberColumns);
                 }
             }
-        else {
-            throw new MyArraySizeException("Размер массива не соответсвует заданному размеру");
+        } else {
+            throw new MyArraySizeException("Количество строк в массиве не равно " + numberLines);
         }
-            return sum;
+        return sum;
     }
 }
