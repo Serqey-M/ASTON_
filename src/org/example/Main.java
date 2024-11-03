f56161c246451d8b0726fcdc817465bbf9e31ad5
Sergey <ranik07@bk.ru>
2024-11-04 01:30:41 +0300

    04/11/2024

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(factorial(-5));
+        System.out.println(factorial(5));
     }
         public static int factorial(int n) {
             int rez = 1;
--- src/org/example/TestF.java
+++ src/org/example/TestF.java
@@ -1,29 +1,17 @@
 package org.example;
 
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.example.Main.factorial;
 
 public class TestF {
-
-    @DataProvider
-    public Object[][] data(){
-        return new Object[][]{
-                {0,1},
-                {1,1},
-                {2,2},
-                {3,6},
-                {4,24},
-                {5,120},
-                {6,720},
-    };
+    @ParameterizedTest
+    @CsvSource({"0,1","1,1","2,2","3,6","4,24","5,120","6,720"})
+    public void testF(int a, int b){
+        Assertions.assertEquals(factorial(a), b);
     }
-
-@Test(dataProvider = "data")
-    public void testFactorial(Integer a, Integer b){
-    Assert.assertEquals(factorial(a),b);
-    }
-
 }
+
+
