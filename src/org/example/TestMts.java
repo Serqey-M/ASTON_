5a677f858bc2d53aaa4cd4c400a8be1149a6f93d
Sergey <ranik07@bk.ru>
2024-11-10 15:16:42 +0300

    10/11/2024

--- src/org/example/TestMts.java
+++ src/org/example/TestMts.java
@@ -1,23 +1,27 @@
 package org.example;
 
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class TestMts {
-    WebDriver driver = new ChromeDriver();
+    static WebDriver driver = new ChromeDriver();
+    WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
-    @BeforeEach
-    public void initDriver(){
+    @BeforeAll
+    public static void initDriver(){
         driver.get("https://www.mts.by/");
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        WebElement buttonCooki = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[2]/div/div[2]/button[2]"));
-        buttonCooki.click();
-
+        WebElement buttonCookie = driver.findElement(By.xpath("/html/body/div[6]/main/div/div[2]/div/div[2]/button[2]"));
+        buttonCookie.click();
     }
 
     @Test
@@ -62,16 +66,33 @@ public class TestMts {
     }
 
     @Test
-    public void linkMoreAboutService() throws InterruptedException {
+    public void linkMoreAboutService() {
         WebElement linkMoreAboutService = driver.findElement(By.xpath("//*[@id=\"pay-section\"]/div/div/div[2]/section/div/a"));
         linkMoreAboutService.click();
-        System.out.println(driver.getCurrentUrl());
+        Assertions.assertEquals(driver.getCurrentUrl(), "https://www.mts.by/help/poryadok-oplaty-i-bezopasnost-internet-platezhey/");
+        driver.get("https://www.mts.by/");
     }
 
-//    @AfterAll
-//    public static void closeDriver(){
-//        driver.quit();
-//    }
+    @Test
+    public void continueButton(){
+        WebElement phoneNumberField = driver.findElement(By.xpath("//*[@id=\"connection-phone\"]"));
+        phoneNumberField.sendKeys("297777777");
+        WebElement amountField = driver.findElement(By.xpath("//*[@id=\"connection-sum\"]"));
+        amountField.sendKeys("10");
+        WebElement continueButton = driver.findElement(By.xpath("//*[@id=\"pay-connection\"]/button"));
+        continueButton.click();
+        WebElement bepaidIframe = driver.findElement(By.xpath("/html/body/div[8]/div/iframe"));
+        driver.switchTo().frame(bepaidIframe);
+        wait.until(ExpectedConditions.visibilityOfElementLocated((By.xpath("/html/body/app-root/div/div/div/app-payment-container/section/div/div/div[1]/span[1]"))));
+        WebElement text = driver.findElement(By.xpath("/html/body/app-root/div/div/div/app-payment-container/section/div/div/div[1]/span[1]"));
+        Assertions.assertEquals(text.getText(), "10.00 BYN");
+        driver.get("https://www.mts.by/");
+    }
+
+    @AfterAll
+    public static void closeDriver(){
+        driver.quit();
+    }
 }
 
 
