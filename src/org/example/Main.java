e9f21098816d0a89f62a7338ffd9f5edfc2c5307
unknown <C:\The Bat!>
2024-11-13 15:35:05 +0300

    коментарий

--- src/org/example/Main.java
+++ src/org/example/Main.java
@@ -3,13 +3,17 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(factorial(-5));
+        System.out.println(factorial(-1));
     }
-        public static int factorial(int n) {
-            int rez = 1;
-            for (int i = 1; i <= n; i++) {
-                rez *= i;
-            }
-            return rez;
+    public static int factorial(int n) {
+        int rez = 1;
+        if(n<0){
+            System.out.println("Невозможно посчитать факториал отрицательного числа");
+            return -1;
+        }
+        for (int i = 1; i <= n; i++) {
+            rez *= i;
+        }
+        return rez;
     }
 }
